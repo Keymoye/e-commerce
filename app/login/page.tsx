2dc25f1d6bb4d21576541562e8b251c1b2ce6213
Keymoye <bowenmaxwell9@gmail.com>
2025-11-29 15:41:40 +0300

    Fix loginPage  page imports

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,6 @@ import LoginPage from "@/components/auth/LoginPage"
 
 export default function Login() {
   return (
-      <section className="max-w-2xl mx-auto mt-10 bg-primary/5 p-6 rounded-2xl shadow-sm">
-        <h1 className="text-2xl font-bold mb-4 text-center">Checkout</h1>
-        <LoginPage/>
-      </section>
+    <LoginPage/>
   );
 }
