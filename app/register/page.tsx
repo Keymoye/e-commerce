388f515600fae5f873f81dcf2c7fdea819c66b6e
Keymoye <bowenmaxwell9@gmail.com>
2025-11-29 15:34:25 +0300

    Fix auth page imports

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,7 @@
-import RegisterPage from "@/components/auth/LoginPage"
+import RegisterPage from "@/components/auth/RegisterPage"
 
 export default function Register() {
   return (
-      <section className="max-w-2xl mx-auto mt-10 bg-primary/5 p-6 rounded-2xl shadow-sm">
-        <h1 className="text-2xl font-bold mb-4 text-center">Checkout</h1>
-        <RegisterPage/>
-      </section>
+    <RegisterPage/>
   );
 }
